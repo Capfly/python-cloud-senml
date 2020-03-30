2bf2b486133040f992656cfb5527c751eb2f3932
wstange <wstange@uni-potsdam.de>
2020-03-30 16:39:36 +0200

    added cbor support

--- cloud_senml/CloudSenML.py
+++ cloud_senml/CloudSenML.py
@@ -1,5 +1,7 @@
-# [{0: "dimmLED", 2: 0, -3: 1579984074}]
+import cbor2
 # [{-3: 1579982564.0, 0: "button", 4: false}, {0: "testProp001", 4: true}, {0: "dimmLED", 2: 25.0}]
+import cbor2
+import json
 
 
 class CloudSenML:
@@ -27,8 +29,24 @@ class CloudSenML:
     def __init__(self):
         self.propList = []
 
-    def print_string(self):
+    def print_list(self):
         print(self.propList)
+
+    def print_cbor(self):
+        print(cbor2.dumps(self.propList))
+
+    def write_to_file(self, file="out.bin"):
+        try:
+            with open(file, "wb") as f:
+                f.write(cbor2.dumps(self.propList))
+                f.close()
+                print("Bytes written to: %s" % file)
+        except FileExistsError:
+            print("Error: File exists.")
+        except IOError:
+            print("Error: IOError")
+        except:
+            print("Error: Unknown error.")
 
     def add_properties(self, BaseVersion=None, BaseName=None, BaseTime=None, BaseUnit=None,
                       BaseValue=None, BaseSum=None, Name=None, Unit=None, Value=None, StringValue=None,
@@ -38,7 +56,8 @@ class CloudSenML:
         dict = {}
 
         for prop in params:
-            if params[prop] is None: continue
+            if params[prop] is None:
+                continue
             else:
                 dict[CloudSenML.CborIntegerMap[prop]] = params[prop]
         self.propList.append(dict)
